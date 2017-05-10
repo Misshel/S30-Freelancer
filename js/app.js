a9df16c371ebee86ea06eef9f596ad5e29292bf2
Misshel <jenmish094@gmail.com>
2017-05-10 13:54:39 -0500

    segundo contenido - portafolio de imagenes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 /*
- * Archivo principal de JS
- */
+* Archivo principal de JS
+*/
+
+var ittem=["","cabin","cake","circus",
+"game", "safe", "submarine"];
+
+for(var i=1;i<ittem.length;i++){
+  var contenedor= document.getElementById("imagenes"); //
+  var div= document.createElement("div");
+  var img= document.createElement("img");
+  var span= document.createElement("span");
+  img.setAttribute("src","assets/images/portfolio/"+ ittem[i] +".png");
+  img.setAttribute("alt",ittem[i]);
+  img.setAttribute("class","imagen");
+
+  span.setAttribute("class","icon-zoomin");
+  img.classList.add("class","modal-img");
+  div.setAttribute("class","div-modal");
+  div.appendChild(img);
+  div.appendChild(span);
+  contenedor.appendChild(div);
+}
+
+
+var modal=document.getElementsByClassName("modal")[0];
+var cerrar=document.getElementsByClassName("boton-cerrar")[0];
+var contenedor=document.getElementById("imagenes");//contenedor de imagenes
+var imagen=document.getElementById("imagen");//contenido del modal
+contenedor.addEventListener('click',function (e) {
+  if (e.target.tagName =="IMG"){
+    modal.style.display="block";
+    cerrar.style.display="inline-block";
+    imagen.src=e.target.src;
+  }
+});
+
+cerrar.addEventListener("click",function(){
+  cerrar.style.display="none";
+  modal.style.display="none";
+});
